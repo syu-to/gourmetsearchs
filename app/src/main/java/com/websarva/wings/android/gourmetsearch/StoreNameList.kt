7a6def60ec52fc765aa21fb73c9c00c7e1850d44
syu-to <kd1333767@st.kobedenshi.ac.jp>
2024-05-19 19:05:41 +0900

    店名検索リスト表示

---
+++ app/src/main/java/com/websarva/wings/android/gourmetsearch/StoreNameList.kt
@@ -0,0 +1,182 @@
+package com.websarva.wings.android.gourmetsearch
+
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
+class StoreNameList : AppCompatActivity() {
+    companion object {
+        private  const val DEBUG_TAG = "AsyncSample"    //ログメッセージ
+        private const val RESTAURANT_URL = "https://webservice.recruit.co.jp/hotpepper/shop/v1/" //店名サーチAPIのアドレス
+        private const val API_ID = "59c533b8ba2a26ed"   //APIキー
+    }
+
+    var keyword = ""
+    var prefectures = ""
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_name_list)
+
+        keyword = intent.getStringExtra("keyword").toString()
+        prefectures = intent.getStringExtra("prefectures").toString()
+
+        val keywordByte = URLEncoder.encode(keyword, StandardCharsets.UTF_8.toString())
+        val prefecturesByte = URLEncoder.encode(prefectures, StandardCharsets.UTF_8.toString())
+
+        val urlFull = "${StoreNameList.RESTAURANT_URL}?key=${StoreNameList.API_ID}&keyword=${keywordByte}%2C${prefecturesByte}&count=30&format=json"
+        println(urlFull)
+        ShopInfo(urlFull)
+
+    }
+
+    @UiThread
+    private fun ShopInfo(urlFull:String){
+        val backgroundReceiver = ShopInfoBackgroundReceiver(urlFull)
+        val executeService = Executors.newSingleThreadExecutor()
+        val future =executeService.submit(backgroundReceiver)
+        val result = future.get()
+        ShowShopInfo(result)
+    }
+
+    private inner class ShopInfoBackgroundReceiver(url: String): Callable<String> {
+        private val _url = url
+        @WorkerThread
+        override fun call(): String{
+            var result = ""
+            //URLオブジェクト生成
+            val url = URL(_url)
+            //URLオブジェクトからHttpURLConnectionオブジェクト取得
+            val con = url.openConnection() as HttpURLConnection
+            //接続に使ってよい時間を設定
+            con.connectTimeout = 1000
+            //データ取得に使ってよい時間
+            con.readTimeout = 1000
+            //HTTP接続メソッドをGETに設定
+            con.requestMethod = "GET"
+            try {
+                //接続。
+                con.connect()
+                //HttpURLConnectionオブジェクトからレスポンスデータ取得
+                val stream = con.inputStream
+                //レスポンスデータであるInputStreamオブジェクトを文字列に変換。
+                result = is2String(stream)
+                //InputStreamオブジェクトを解放
+                stream.close()
+            }
+            catch (ex: SocketTimeoutException){
+                Log.w(StoreNameList.DEBUG_TAG,"通信タイムアウト",ex)
+            }
+            con.disconnect()
+            //WebAPIに接続するコード
+            return  result
+        }
+
+        private fun is2String(stream: InputStream): String{
+            val sb = StringBuilder()
+            val reader =  BufferedReader(
+                InputStreamReader(
+                stream, StandardCharsets.UTF_8)
+            )
+            var line = reader.readLine()
+            while (line != null){
+                sb.append(line)
+                line = reader.readLine()
+            }
+            reader.close()
+            return sb.toString()
+        }
+
+    }
+
+    @UiThread
+    private fun ShowShopInfo(result: String){
+        val alllist: MutableList<MutableMap<String, String>> = mutableListOf()
+        val namelist:MutableList<String> = mutableListOf()
+        val storenameList = findViewById<ListView>(R.id.storenamelist)
+        //ルートオブジェクトを生成
+        val rootJSON = JSONObject(result)
+        //レストラン情報JSON配列オブジェクトを取得
+        val ShopJSONArrays = rootJSON.getJSONObject("results")
+        println(ShopJSONArrays.length())
+        if(ShopJSONArrays.length() != 2) {
+            val ShopJSONArray = ShopJSONArrays.getJSONArray("shop")
+            if (ShopJSONArray.length() != 0) {
+                var ShopJSON = ShopJSONArray.getJSONObject(0)
+                var name = ShopJSON.getString("name")
+                var id = ShopJSON.getString("id")
+
+
+                var menu = mutableMapOf("name" to name, "id" to id)
+                alllist.add(menu)
+                var count = 1
+                var size = ShopJSONArray.length()
+                while (count <= size - 1) {
+                    //すべての情報を取得取得
+                    ShopJSON = ShopJSONArray.getJSONObject(count)
+                    var name = ShopJSON.getString("name")
+                    var id = ShopJSON.getString("id")
+                    var menu = mutableMapOf("name" to name, "id" to id)
+                    alllist.add(menu)
+                    count += 1
+                }
+
+                if (alllist.size != 0) {
+                    //第4引数form用データの用意
+                    val from = arrayOf("name")
+                    //第5引数to用データ
+                    val to = intArrayOf(android.R.id.text1)
+                    //adapter作成
+                    val adapter =
+                        SimpleAdapter(this@StoreNameList, alllist, android.R.layout.simple_list_item_activated_2, from, to)
+                    storenameList.adapter = adapter
+                } else {
+                    Snackbar.make(
+                        storenameList,
+                        "検索結果が0件でした",
+                        Snackbar.LENGTH_SHORT
+                    )    //バーで追加したことを知らせる
+                        .setAction("戻る") { finish() }  //戻るボタン
+                        .setActionTextColor(Color.BLUE)   //テキストカラー
+                        .show()
+                }
+            } else {
+                Snackbar.make(
+                    storenameList,
+                    "検索結果が0件でした",
+                    Snackbar.LENGTH_SHORT
+                )    //バーで追加したことを知らせる
+                    .setAction("戻る") { finish() }  //戻るボタン
+                    .setActionTextColor(Color.BLUE)   //テキストカラー
+                    .show()
+            }
+        }
+        else
+        {
+            Snackbar.make(
+                storenameList,
+                "検索結果が30件超えでした。キーワードを増やしてみてください",
+                Snackbar.LENGTH_SHORT
+            )    //バーで追加したことを知らせる
+                .setAction("戻る") { finish() }  //戻るボタン
+                .setActionTextColor(Color.BLUE)   //テキストカラー
+                .show()
+        }
+    }
+}
--- app/src/main/java/com/websarva/wings/android/gourmetsearch/StoreNameSearch.kt
+++ app/src/main/java/com/websarva/wings/android/gourmetsearch/StoreNameSearch.kt
@@ -1,11 +1,119 @@
 package com.websarva.wings.android.gourmetsearch
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class StoreNameSearch : AppCompatActivity() {
+    var prefectures = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_store_name_search)
+
+        val prefecturesItem = arrayOf(
+            "東京",
+            "大阪",
+            "北海道",
+            "青森",
+            "秋田",
+            "岩手",
+            "山形",
+            "宮城",
+            "福島",
+            "群馬",
+            "栃木",
+            "茨城",
+            "埼玉",
+            "神奈川",
+            "千葉",
+            "山梨",
+            "静岡",
+            "新潟",
+            "富山",
+            "石川",
+            "福井",
+            "長野",
+            "岐阜",
+            "愛知",
+            "三重",
+            "滋賀",
+            "奈良",
+            "和歌山",
+            "京都",
+            "兵庫",
+            "香川",
+            "徳島",
+            "高知",
+            "愛媛",
+            "鳥取",
+            "島根",
+            "岡山",
+            "広島",
+            "山口",
+            "福岡",
+            "佐賀",
+            "長崎",
+            "大分",
+            "熊本",
+            "宮崎",
+            "鹿児島",
+            "沖縄"
+        )
+
+        val adapter = ArrayAdapter(
+            applicationContext,
+            android.R.layout.simple_spinner_item,
+            prefecturesItem
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+        var prefecturesSp = findViewById<Spinner>(R.id.prefectures)
+        var prefecturesTx = findViewById<TextView>(R.id.prefecturesText)
+        prefecturesSp.adapter = adapter
+        prefecturesSp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            //　アイテムが選択された時
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?, position: Int, id: Long
+            ) {
+                val spinnerParent = parent as Spinner
+                val item = spinnerParent.selectedItem as String
+                // View Binding
+                prefecturesTx.text = item
+                prefectures = prefecturesItem[position]
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //
+                prefectures = "東京"
+            }
+
+        }
+
+        val listener = searchListener()
+        val storenamesearchbutton = findViewById<Button>(R.id.Keywordsearch)
+        storenamesearchbutton.setOnClickListener(listener)
+    }
+
+    private inner class searchListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            //idのR値に応じて処理を分岐する
+            var keyword = findViewById<EditText>(R.id.KeywordText)
+            var keyText = keyword.text.toString()
+            keyText = keyText.replace("\\s+".toRegex(), "AAA")
+            if(keyword.text.toString() == ""){
+                keyText = "検索された文字が存在しません。"
+            }
+            when (view.id) {
+                R.id.Keywordsearch -> {
+                    val intent = Intent(this@StoreNameSearch, StoreNameList::class.java)
+                    intent.putExtra("keyword",keyText.toString())        //半径データを渡す
+                    intent.putExtra("prefectures",prefectures.toString())
+                    startActivity(intent)   //飛ぶ
+                }
+            }
+        }
     }
 }
