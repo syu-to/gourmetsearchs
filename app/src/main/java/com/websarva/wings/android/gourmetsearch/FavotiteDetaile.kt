6c5d987133e70f0b8b8875128d667135661ef831
syu-to <kd1333767@st.kobedenshi.ac.jp>
2024-05-19 14:35:58 +0900

    こみっと

--- app/src/main/java/com/websarva/wings/android/gourmetsearch/FavotiteDetaile.kt
+++ app/src/main/java/com/websarva/wings/android/gourmetsearch/FavotiteDetaile.kt
@@ -66,7 +66,6 @@ class FavotiteDetaile : AppCompatActivity() {
         if(networkCapabilities != null) {
             val imageTask: RestaurantDetail.GetImage = RestaurantDetail.GetImage(photoimage)
             imageTask.execute(photo)
-
         }
     }
 
